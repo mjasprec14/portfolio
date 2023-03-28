6d1a9e625349196c308b31ca489f5d24c274c520
mjasprec <michaeljosephasprec1@gmail.com>
2023-03-28 23:56:08 +0800

    Added necessary imports and components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,27 @@
 import Head from 'next/head';
-import {Header, Hero} from "@/components";
+import { Header, Hero } from '@/components';
 import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen'>
       <Head>
         <title>MJ Asprec Portfolio</title>
-        <meta name="description" content="MJ Asprec Portfolio" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name='description'
+          content='MJ Asprec Portfolio'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1'
+        />
       </Head>
 
-      
       <Header />
 
-      {/* HERO */}
-      <section id="hero">
+      <section id='hero'>
         <Hero />
       </section>
 
@@ -30,6 +34,6 @@ export default function Home() {
       {/* PROJECTS */}
 
       {/* CONTACT ME */}
-    </>
-  )
+    </div>
+  );
 }
