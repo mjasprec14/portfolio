d8665856dec3ee0b645360c1e82ddc7e046e9c56
mjasprec <michaeljosephasprec1@gmail.com>
2023-03-28 18:10:35 +0800

    framer motion on header content

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
+
 
 type Props = {}
 
 export default function Header({}: Props) {
   return (
     <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
-        <div className='flex flex-row items-center'>
+        <motion.div 
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1.5
+        }}
+        className='flex flex-row items-center'
+        >
             {/* SOCIAL ICONS */}
 
             <SocialIcon url="https://twitter.com/MjAsprec" fgColor='gray' bgColor='transparent'/>
@@ -18,12 +35,27 @@ export default function Header({}: Props) {
             <SocialIcon url="https://read.cv/mjasprec" fgColor='gray' bgColor='transparent'/>
 
             <SocialIcon url="https://github.com/mjasprec14" fgColor='gray' bgColor='transparent'/>
-        </div>
+        </motion.div>
 
-        <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+        <motion.div 
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1
+        }}
+        transition={{
+          duration: 1.5
+        }}
+        className='flex flex-row items-center text-gray-300 cursor-pointer'
+        >
             <SocialIcon className='cursor-pointer' network='email' fgColor='gray' bgColor='transparent'/>
             <p className="uppercase hidden md:inline-flex text-sm text-gray-400">get in touch</p>
-        </div>
+        </motion.div>
     </header>
   )
 }
