e9208afafc849f2dfe0ddf37d376e9459e80b469
mjasprec <michaeljosephasprec1@gmail.com>
2023-03-28 23:55:14 +0800

    Completed hero section initial setup

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
 
-type Props = {}
+type Props = {};
 
 export default function Hero({}: Props) {
+  const [text, count] = useTypewriter({
+    words: [
+      "Hi, The Name's MJ Asprec",
+      'I was on Hiatus..',
+      "But, I'm back hungrier",
+      'with more motivation',
+      "let's go!",
+      '<ShowMeTheMoney/>',
+    ],
+    loop: true,
+    delaySpeed: 2000,
+  });
+
   return (
-    <div>Hero!!</div>
-  )
+    <div>
+      <h1>
+        <span>{text}</span>
+        <Cursor cursorColor='#F7AB0A' />
+      </h1>
+    </div>
+  );
 }
