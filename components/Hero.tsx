e1a4b596b7e09c889c28fbd23281388acb72c2ee
mjasprec14 <michaeljosephasprec1@gmail.com>
2023-03-31 20:21:14 +0800

    trying out nextjs image component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Cursor, useTypewriter } from 'react-simple-typewriter';
 import BackgroundCircles from './BackgroundCircles';
+import Image from 'next/image';
+import Me from '@/assets/me.png';
 
 type Props = {};
 
@@ -22,6 +24,10 @@ export default function Hero({}: Props) {
   return (
     <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
       <BackgroundCircles />
+      <Image
+        src={Me}
+        alt='MJ Asprec photo'
+      />
       <h1>
         <span>{text}</span>
         <Cursor cursorColor='#F7AB0A' />
