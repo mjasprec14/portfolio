3939fb2fd977978999e63876fe120e1948124a33
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-05 19:23:14 +0800

    fix: Skill components text copy and animation

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -13,6 +13,7 @@ export default function Skill({ directionLeft }: Props) {
       <motion.img
         initial={{
           x: directionLeft ? -200 : 200,
+          opacity: 0,
         }}
         transition={{
           duration: 1,
@@ -21,11 +22,14 @@ export default function Skill({ directionLeft }: Props) {
           opacity: 1,
           x: 0,
         }}
+        viewport={{
+          once: true,
+        }}
         src={htmlIcon.src}
-        className='rounded-full border border-gray-500 object-cover w-24 h-24 xl:h-28 xl:w-28 filter group-hover:grayscale transition duration-300 ease-in-out'
+        className='rounded-full border border-gray-500 object-cover w-20 h-20 xl:h-24 xl:w-24 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
 
-      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 xl:h-28 xl:w-28 rounded-full z-0'>
+      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-20 w-20 xl:h-24 xl:w-24 rounded-full z-0'>
         <div className='flex items-center justify-center h-full'>
           <p className='text-black text-3xl font-bold opacity-100'>100%</p>
         </div>
