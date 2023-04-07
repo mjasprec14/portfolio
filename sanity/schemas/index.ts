d0b81ae51b6b318d1a1e615dbf668cd4e1721224
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-07 16:56:17 +0800

    Revert "feat: schema types import export"
    This reverts commit 39797d3ede09d4b7a0e2ed07d1098bbe375d4138.

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,7 +1,3 @@
 import pageInfo from './pageInfo'
-import experience from './experience'
-import project from './project'
-import skill from './skill'
-import social from './social'
 
-export const schemaTypes = [pageInfo, experience, project, skill, social]
+export const schemaTypes = [pageInfo]
