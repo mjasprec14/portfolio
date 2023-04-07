ca90956cad6b5ff3c5fc44a62a9f7af3779440a8
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-07 17:44:13 +0800

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,6 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './src/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 };
