38d3a28b8170f045c408baa8a998da0324382da4
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-08 16:20:58 +0800

    feat: about section photo

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import AboutPhoto from '@/assets/about.jpg';
 
 type Props = {};
 
 const About = (props: Props) => {
-  return <div>About</div>;
+  return (
+    <div className='sectionContainer'>
+      <h3 className='sectionHeader'>About</h3>
+
+      <motion.img
+        initial={{ x: -200, opacity: 0 }}
+        whileInView={{ x: 0, opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        viewport={{ once: true }}
+        src={AboutPhoto.src}
+        className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:h-96 md:w-64 xl:h-[600px] xl:w-[500px]'
+      />
+    </div>
+  );
 };
 
 export default About;
