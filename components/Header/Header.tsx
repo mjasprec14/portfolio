4cdae6ac9593315df7595c0541cb1aaa4e9231e4
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-08 23:00:08 +0800

    fix: Linked contact icon to contact section

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const Header = (props: Props) => {
   return (
-    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center'>
+    <header className='sticky top-0 z-20 mx-auto flex max-w-7xl items-start justify-between p-5 xl:items-center'>
       <div className='flex flex-row items-center'>
         <motion.span
           initial={{ x: -200, scale: 0.5 }}
@@ -69,13 +69,14 @@ const Header = (props: Props) => {
         </motion.span>
       </div>
 
-      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+      <div className='flex cursor-pointer flex-row items-center text-gray-300'>
         <motion.span
           initial={{ opacity: 0, x: 200, scale: 0.5 }}
           animate={{ opacity: 1, x: 0, scale: 1 }}
           transition={{ duration: 1.5 }}
         >
           <SocialIcon
+            url='/#contact'
             className='cursor-pointer'
             network='email'
             fgColor='gray'
@@ -84,7 +85,7 @@ const Header = (props: Props) => {
         </motion.span>
 
         <motion.p
-          className='hidden md:inline-flex text-sm text-gray-400 uppercase'
+          className='hidden text-sm uppercase text-gray-400 md:inline-flex'
           initial={{ opacity: 0, x: -200, scale: 0.5 }}
           animate={{ opacity: 1, x: 0, scale: 1 }}
           transition={{ duration: 1 }}
