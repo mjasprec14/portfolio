44e187dd6379e4ef74629de4cacf2a7e348a2186
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-08 16:52:38 +0800

    feat: WorkExperience section initial

--- components/WorkExperience/WorkExperience.tsx
+++ components/WorkExperience/WorkExperience.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import ExperienceCard from '../ExperienceCard/ExperienceCard';
 
 type Props = {};
 
 const WorkExperience = (props: Props) => {
-  return <div>WorkExperience</div>;
+  return (
+    <motion.div className='sectionContainer overflow-hidden max-w-full'>
+      <h3 className='sectionHeader'>Experience</h3>
+
+      <div>
+        <ExperienceCard />
+        <ExperienceCard />
+        <ExperienceCard />
+      </div>
+    </motion.div>
+  );
 };
 
 export default WorkExperience;
