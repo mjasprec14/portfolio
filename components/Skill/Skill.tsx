80038b0d8962c0641eef2545afdc90d72637d7ff
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-08 19:06:10 +0800

    feat: completed skill section styling

--- components/Skill/Skill.tsx
+++ components/Skill/Skill.tsx
@@ -18,6 +18,12 @@ const Skill = (directionLeft: Props) => {
         alt='NextJs Icon'
         className='h-16 w-16 rounded-full border border-gray-500 object-cover object-center xl:h-24 xl:w-24 filter group-hover:grayscale transition duration-300 ease-in-out'
       />
+
+      <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-16 w-16 xl:h-24 xl:w-24 rounded-full z-0'>
+        <div className='flex items-center justify-center h-full'>
+          <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+        </div>
+      </div>
     </div>
   );
 };
