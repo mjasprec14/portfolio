8e8f223cdb151d4a7b15bb3bfd4d02b496b34376
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-10 02:06:49 +0800

    feat: Skill Schema

--- sanity/schemas/skill.ts
+++ sanity/schemas/skill.ts
@@ -1,30 +1,20 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'skill',
-  title: 'Skill',
+  name: 'social',
+  title: 'Social',
   type: 'document',
   fields: [
     defineField({
       name: 'title',
       title: 'Title',
-      description: 'Title of the skill',
+      description: 'Platform for social media',
       type: 'string',
     }),
     defineField({
-      name: 'progress',
-      title: 'Progress',
-      type: 'number',
-      description: 'Progress of skill from 0 to 100%',
-      validation: (Rule) => Rule.min(0).max(100),
-    }),
-    defineField({
-      name: 'image',
-      title: 'Image',
-      type: 'image',
-      options: {
-        hotspot: true,
-      },
+      name: 'url',
+      title: 'Url',
+      type: 'url',
     }),
   ],
 })
