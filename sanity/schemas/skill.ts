2826d43194c1e41a3fd9b9ddeca45d0aa780548b
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-10 02:11:04 +0800

    fix: Corrected field types

--- sanity/schemas/skill.ts
+++ sanity/schemas/skill.ts
@@ -1,20 +1,30 @@
 import {defineField, defineType} from 'sanity'
 
 export default defineType({
-  name: 'social',
-  title: 'Social',
+  name: 'skill',
+  title: 'Skill',
   type: 'document',
   fields: [
     defineField({
       name: 'title',
       title: 'Title',
-      description: 'Platform for social media',
+      description: 'Title of skill',
       type: 'string',
     }),
     defineField({
-      name: 'url',
-      title: 'Url',
-      type: 'url',
+      name: 'progress',
+      title: 'Progress',
+      type: 'number',
+      description: 'Progress of skill from 0 to 100%',
+      validation: (Rule) => Rule.min(0).max(100),
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
     }),
   ],
 })
