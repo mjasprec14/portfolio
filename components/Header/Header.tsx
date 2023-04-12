6dcb9d2ee74135a3902c0835c162221415501ba5
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-12 20:39:02 +0800

    feat: fetch live data from socials

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import { SocialIcon } from 'react-social-icons';
 import { Social } from '@/typings';
@@ -8,69 +8,56 @@ type Props = {
 };
 
 const Header = ({ socials }: Props) => {
-  console.log(socials);
+  const animationValues = [
+    {
+      initial: { x: -200, scale: 0.5 },
+      animate: {
+        x: 0,
+        scale: 1,
+      },
+      transition: { duration: 1 },
+    },
+    {
+      initial: { y: -200, scale: 0.5 },
+      animate: { y: 0, scale: 1 },
+      transition: { duration: 1.5 },
+    },
+    {
+      initial: { opacity: 0, scale: 0.5 },
+      animate: { opacity: 1, scale: 1 },
+      transition: { duration: 1.8 },
+    },
+    {
+      initial: { opacity: 0, y: 200, scale: 0.5 },
+      animate: { opacity: 1, y: 0, scale: 1 },
+      transition: { duration: 0.8 },
+    },
+    {
+      initial: { opacity: 0, x: 200, scale: 0.5 },
+      animate: { opacity: 1, x: 0, scale: 1 },
+      transition: { duration: 1.5 },
+    },
+  ];
+
   return (
     <header className='sticky top-0 z-20 mx-auto flex max-w-7xl items-start justify-between p-5 xl:items-center'>
       <div className='flex flex-row items-center'>
-        <motion.span
-          initial={{ x: -200, scale: 0.5 }}
-          animate={{ x: 0, scale: 1 }}
-          transition={{ duration: 1 }}
-        >
-          <SocialIcon
-            url='https://www.linkedin.com/in/michael-joseph-asprec-254071251/'
-            fgColor='gray'
-            bgColor='transparent'
-          />
-        </motion.span>
-
-        <motion.span
-          initial={{ y: -200, scale: 0.5 }}
-          animate={{ y: 0, scale: 1 }}
-          transition={{ duration: 1.5 }}
-        >
-          <SocialIcon
-            url='https://github.com/mjasprec14'
-            fgColor='gray'
-            bgColor='transparent'
-          />
-        </motion.span>
-
-        <motion.span
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 1.8 }}
-        >
-          <SocialIcon
-            url='https://www.facebook.com/MichaelJosephAsprec/'
-            fgColor='gray'
-            bgColor='transparent'
-          />
-        </motion.span>
-
-        <motion.span
-          initial={{ opacity: 0, y: 200, scale: 0.5 }}
-          animate={{ opacity: 1, y: 0, scale: 1 }}
-          transition={{ duration: 0.8 }}
-        >
-          <SocialIcon
-            url='https://twitter.com/MjAsprec'
-            fgColor='gray'
-            bgColor='transparent'
-          />
-        </motion.span>
-
-        <motion.span
-          initial={{ opacity: 0, x: 200, scale: 0.5 }}
-          animate={{ opacity: 1, x: 0, scale: 1 }}
-          transition={{ duration: 1.5 }}
-        >
-          <SocialIcon
-            url='https://portfolio-git-main-mjasprec14.vercel.app/'
-            fgColor='gray'
-            bgColor='transparent'
-          />
-        </motion.span>
+        {socials.length > 0
+          ? socials.map((social, idx) => (
+              <motion.span
+                key={social._id}
+                initial={animationValues[idx].initial}
+                animate={animationValues[idx].animate}
+                transition={animationValues[idx].transition}
+              >
+                <SocialIcon
+                  url={social.url}
+                  fgColor='gray'
+                  bgColor='transparent'
+                />
+              </motion.span>
+            ))
+          : null}
       </div>
 
       <div className='flex cursor-pointer flex-row items-center text-gray-300'>
