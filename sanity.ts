c56dc84260f3aae0441d4bd99f832f7ce7e4472f
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-12 21:53:44 +0800

    fix: api version issue

--- sanity.ts
+++ sanity.ts
@@ -4,7 +4,7 @@ import { createClient } from 'next-sanity';
 export const config = {
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'production',
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  apiVersion: '2022-04-12' || '1',
   useCdn: process.env.NODE_ENV === 'production',
 };
 
