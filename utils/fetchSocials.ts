0e4260ee7a006d0eba2291c9aaac63463abe383c
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-12 20:39:12 +0800

    fix: remove axios

--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -1,16 +1,10 @@
 import { Social } from '@/typings';
-import axios from 'axios';
 
 const fetchSocials = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`);
 
   const data = await res.json();
   const socials: Social[] = data.socials;
-
-  // axios
-  //   .get(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`)
-  //   .then((res) => console.log(res))
-  //   .catch((err) => console.log(err));
 
   console.log('fetching socials...', socials);
 
