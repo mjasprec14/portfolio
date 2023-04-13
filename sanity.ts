a42381b91c6ea9bb5679fe6f0efec31091132764
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-13 21:39:48 +0800

    fix: incorrect config

--- sanity.ts
+++ sanity.ts
@@ -2,8 +2,8 @@ import createImageUrlBuilder from '@sanity/image-url';
 import { createClient } from 'next-sanity';
 
 export const config = {
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'production',
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   apiVersion: '2022-04-12' || '1',
   useCdn: process.env.NODE_ENV === 'production',
 };
