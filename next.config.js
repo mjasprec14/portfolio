e9e449b403102a3993796aca685db9aeb38f7c66
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-18 19:38:57 +0800

    sanity backend initial setup

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-const isProd = process.env.NODE.ENV === 'production';
-
 const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: isProd
-    ? 'https://mj-asprec-portfolio.vercel.app'
-    : 'http://localhost:3000',
-  images: {
-    domains: ['cdn.sanity.io'],
+  experimental: {
+    appDir: true,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
