b811e4e864d5dc7a3533c24ee4e57fedff87e201
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-18 23:52:33 +0800

    fix: remove Head component

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,12 @@
 import { getSocials } from '@utils';
-import Head from 'next/head';
 import { Header } from '@/src/components';
 
 export default async function Home() {
   const socials = await getSocials();
 
-  console.log(socials);
   return (
     <main className='z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-[rgb(36,36,36)] text-white scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
-      <Head>
-        <title>MJ Asprec</title>
-      </Head>
-      <Header />
+      <Header socials={socials} />
     </main>
   );
 }
