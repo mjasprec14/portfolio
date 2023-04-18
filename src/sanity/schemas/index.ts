b4c90310b617ee449b440fbe95c53b4569bd2323
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-18 20:36:44 +0800

    feat: Sanity schemas and default export

---
+++ src/sanity/schemas/index.ts
@@ -0,0 +1,5 @@
+export { default as experience } from './experience';
+export { default as pageInfo } from './pageInfo';
+export { default as project } from './project';
+export { default as skill } from './skill';
+export { default as social } from './social';
---
+++ src/sanity/schemas/project.ts
@@ -0,0 +1,54 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: { source: 'name' },
+    }),
+    defineField({
+      name: 'title',
+      title: 'Title',
+      description: 'Title of the project',
+      type: 'string',
+    }),
+    defineField({
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: { hotspot: true },
+    }),
+    defineField({
+      name: 'summary',
+      title: 'Summary',
+      type: 'array',
+      of: [{ type: 'block' }],
+    }),
+    defineField({
+      name: 'technologies',
+      title: 'Technologies',
+      type: 'array',
+      of: [{ type: 'reference', to: { type: 'skill' } }],
+    }),
+    defineField({
+      name: 'linkToBuild',
+      title: 'LinkToBuild',
+      type: 'url',
+    }),
+    defineField({
+      name: 'url',
+      title: 'Url',
+      type: 'url',
+    }),
+  ],
+});
