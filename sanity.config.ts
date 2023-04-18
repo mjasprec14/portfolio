7e836505b443f9533d6b9020cf4228f2166391df
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-18 21:06:12 +0800

    feat: applied schemas to config file

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
+import schemas from '@schemas';
 
 const config = defineConfig({
   projectId: 'd5grszkw',
@@ -9,6 +10,7 @@ const config = defineConfig({
   apiVersion: '2023-18-04',
   basePath: '/admin',
   plugins: [deskTool(), visionTool()],
+  schema: { types: schemas },
 });
 
 export default config;
