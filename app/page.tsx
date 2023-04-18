1ef62c08bdf5fe575700ad9292c3a789df7d1d4c
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-18 23:34:09 +0800

    feat: Home page initial setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import { getSocials } from '@utils';
+import Head from 'next/head';
+import { Header } from '@/src/components';
 
 export default async function Home() {
   const socials = await getSocials();
 
   console.log(socials);
   return (
-    <main>
-      <h1>Socials</h1>
+    <main className='z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll bg-[rgb(36,36,36)] text-white scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
+      <Head>
+        <title>MJ Asprec</title>
+      </Head>
+      <Header />
     </main>
   );
 }
