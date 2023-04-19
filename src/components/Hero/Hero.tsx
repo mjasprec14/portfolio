8d691a1733328c7c92033a051e01514f867d6bee
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-19 22:39:46 +0800

    feat: Hero animation

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,9 @@
+'use client';
 import React from 'react';
 import Image from 'next/image';
 import { PageInfo } from '@/src/typings/typings';
 import { urlFor } from '@/src/sanity/sanity.utils';
+import { motion } from 'framer-motion';
 
 type Props = {
   pageInfo: PageInfo;
@@ -11,7 +13,12 @@ const Hero = ({ pageInfo }: Props) => {
   return (
     <section id='home'>
       <div className='h-screen flex flex-col -mt-16 items-center justify-center gap-10 md:flex-row md:-mt-32 md:px-12'>
-        <div className='flex items-center justify-center'>
+        <motion.div
+          initial={{ opacity: 0, y: -200 }}
+          animate={{ opacity: [0, 0, 0.5, 0.8, 1], y: 0 }}
+          transition={{ duration: 2 }}
+          className='flex items-center justify-center'
+        >
           <Image
             src={urlFor(pageInfo?.heroImage).url()}
             alt={`${pageInfo?.name} hero image`}
@@ -19,18 +26,28 @@ const Hero = ({ pageInfo }: Props) => {
             width={100}
             className='h-32 w-32 rounded-full object-cover'
           />
-        </div>
+        </motion.div>
 
         <div className='flex flex-col justify-center items-center text-center md:space-y-5'>
-          <h1 className='text-3xl md:text-7xl font-extrabold'>
+          <motion.h1
+            initial={{ opacity: 0, x: -200 }}
+            animate={{ opacity: [0, 0, 0, 0.5, 1], x: 0 }}
+            transition={{ duration: 3.5 }}
+            className='text-3xl md:text-7xl font-extrabold'
+          >
             Hello I&apos;m{' '}
             <span className='bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent '>
               {pageInfo?.name}
             </span>
-          </h1>
-          <h3 className='text-xs tracking-[10px] md:text-sm md:tracking-[20px] uppercase text-gray-500 '>
+          </motion.h1>
+          <motion.h3
+            initial={{ opacity: 0, x: 200 }}
+            animate={{ opacity: [0, 0, 0, 0.5, 1], x: 0 }}
+            transition={{ duration: 3.5 }}
+            className='text-xs tracking-[10px] md:text-sm md:tracking-[20px] uppercase text-gray-500 '
+          >
             {pageInfo?.role}
-          </h3>
+          </motion.h3>
         </div>
       </div>
     </section>
