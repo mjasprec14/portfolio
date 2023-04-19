b9119fe30a74015453b280b02c70a61bc1ba4900
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-19 22:11:05 +0800

    feat: urlFor image builder

---
+++ src/sanity/sanity.utils.ts
@@ -0,0 +1,13 @@
+import { createClient } from 'next-sanity';
+import createImageUrlBuilder from '@sanity/image-url';
+
+export const config = {
+  projectId: 'd5grszkw',
+  dataset: 'production',
+  apiVersion: '2023-04-18',
+};
+
+export const sanityClient = createClient(config);
+
+export const urlFor = (source: any) =>
+  createImageUrlBuilder(config).image(source);
