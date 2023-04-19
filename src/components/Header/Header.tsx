0960618bae8a0e478e8d0938e59299c646fb5904
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-19 12:59:12 +0800

    feat: Dark theme controls

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,15 @@ import React from 'react';
 import { animationValues } from './Header.utils';
 import { motion } from 'framer-motion';
 import { SocialIcon } from 'react-social-icons';
+import { RiMoonFill, RiSunLine } from 'react-icons/ri';
+import { useTheme } from 'next-themes';
 
 type Props = {};
 
 const Header = ({ socials }: any) => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
   return (
     <header className='sticky top-0 z-20 mx-auto flex max-w-7xl items-start justify-between p-5 xl:items-center'>
       <div className='flex flex-row items-center'>
@@ -26,8 +31,8 @@ const Header = ({ socials }: any) => {
         ))}
       </div>
 
-      <div className='flex cursor-pointer flex-row items-center text-gray-300'>
-        <motion.span
+      <div className='flex flex-row cursor-pointer gap-2 items-center text-gray-300'>
+        <motion.i
           initial={{ opacity: 0, x: 200, scale: 0.5 }}
           animate={{ opacity: 1, x: 0, scale: 1 }}
           transition={{ duration: 1.5 }}
@@ -39,16 +44,40 @@ const Header = ({ socials }: any) => {
             fgColor='gray'
             bgColor='transparent'
           />
-        </motion.span>
+        </motion.i>
 
-        <motion.p
-          className='hidden text-sm uppercase text-gray-400 md:inline-flex'
-          initial={{ opacity: 0, x: -200, scale: 0.5 }}
-          animate={{ opacity: 1, x: 0, scale: 1 }}
-          transition={{ duration: 1 }}
-        >
-          get in touch
-        </motion.p>
+        {currentTheme === 'dark' ? (
+          <motion.span
+            className='hidden text-sm uppercase text-gray-400 md:inline-flex'
+            initial={{ opacity: 0, x: -200, scale: 0.5 }}
+            animate={{ opacity: 1, x: 0, scale: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <button
+              onClick={() => setTheme('light')}
+              className='rounded-xl'
+            >
+              <RiSunLine
+                size={20}
+                color='gray'
+              />
+            </button>
+          </motion.span>
+        ) : (
+          <motion.span
+            className='hidden text-sm uppercase text-[#808080] md:inline-flex'
+            initial={{ opacity: 0, x: -200, scale: 0.5 }}
+            animate={{ opacity: 1, x: 0, scale: 1 }}
+            transition={{ duration: 1 }}
+          >
+            <button
+              onClick={() => setTheme('dark')}
+              className='rounded-xl'
+            >
+              <RiMoonFill size={20} />
+            </button>
+          </motion.span>
+        )}
       </div>
     </header>
   );
