75c16481afa0c52a99c3a29a62bee85387eba99f
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-19 20:54:02 +0800

    feat: import export navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { getSocials } from '@utils';
-import { Header } from '@/src/components';
+import { Header, Navbar } from '@/src/components';
 
 export default async function Home() {
   const socials = await getSocials();
 
   return (
-    <main className='z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll   scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 dark:bg-[rgb(36,36,36)] dark:text-white'>
+    <main className='z-0 h-screen snap-y snap-mandatory overflow-x-hidden overflow-y-scroll scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 dark:bg-[rgb(36,36,36)] dark:text-white'>
       <Header socials={socials} />
+      <Navbar />
     </main>
   );
 }
