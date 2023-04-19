8c2f9d2ac17e0e704ec5b14cdaef797d7279edae
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-19 11:43:24 +0800

    feat: tailwind config darkmode class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
