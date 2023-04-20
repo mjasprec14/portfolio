95a35e98a85ead76567de5d866680b9139222181
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-21 05:53:01 +0800

    fix: transition effect bg color

--- src/components/TransitionEffect/TransitionEffect.tsx
+++ src/components/TransitionEffect/TransitionEffect.tsx
@@ -7,20 +7,20 @@ const TransitionEffect = (props: Props) => {
   return (
     <>
       <motion.div
-        className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-gray-700'
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-blue-600'
         initial={{ x: '100%', width: '100%' }}
         animate={{ x: '0%', width: '0%' }}
         exit={{ x: ['0%', '100%'], width: ['0%', '100%'] }}
         transition={{ duration: 0.8, ease: 'easeInOut' }}
       ></motion.div>
       <motion.div
-        className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-orange-500'
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-red-500'
         initial={{ x: '100%', width: '100%' }}
         animate={{ x: '0%', width: '0%' }}
         transition={{ delay: 0.2, duration: 0.8, ease: 'easeInOut' }}
       ></motion.div>
       <motion.div
-        className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-blue-900'
+        className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#F7AB0A]'
         initial={{ x: '100%', width: '100%' }}
         animate={{ x: '0%', width: '0%' }}
         transition={{ delay: 0.4, duration: 0.8, ease: 'easeInOut' }}
