380373e64c6919b8a1fb5351bcb9abf57e917afd
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-20 18:03:33 +0800

    fix: about mobile view

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -16,19 +16,20 @@ type Props = {
 const About = ({ pageInfo }: Props) => {
   return (
     <section id='about'>
+      <TransitionEffect />
       <motion.div
         initial={{ y: 150, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ duration: 1 }}
-        className='h-screen max-w-7xl mx-auto flex flex-col px-8 items-center space-y-6 md:space-y-0 md:justify-between md:px-20 md:flex-row'
+        className='h-screen max-w-7xl mx-auto flex flex-col px-8 items-center justify-center  md:space-y-0 md:justify-between md:px-20 md:flex-row'
       >
-        <div className='w-full md:w-1/2'>
+        <div className='w-full flex items-center justify-center pt-8 md:pt-0 md:w-1/2'>
           <Image
             src={urlFor(pageInfo?.profilePic).url()}
             alt={`${pageInfo.name} profile photo`}
             height={1200}
             width={1200}
-            className='md:w-5/6 rounded-2xl'
+            className='w-full h-5/6 object-cover md:w-5/6 rounded-2xl'
           />
         </div>
 
@@ -39,7 +40,7 @@ const About = ({ pageInfo }: Props) => {
           <motion.p
             variants={quote}
             initial='initial'
-            animate='animate'
+            whileInView='animate'
             className='text-sm md:text-xl md:font-thin'
           >
             {pageInfo?.backgroundInformation.split(' ').map((word, idx) => (
@@ -58,7 +59,7 @@ const About = ({ pageInfo }: Props) => {
               href='/public/mjcv.pdf'
               target='_blank'
               download={true}
-              className='flex items-center bg-[rgb(36,36,36)] text-white/80 p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-white/80 hover:text-[rgb(36,36,36)] border-2 border-solid border-transparent hover:border-[rgb(36,36,36)] dark:border-2 dark:border-solid dark:border-white/80 transition space-x-2'
+              className='flex items-center bg-[rgb(36,36,36)] text-sm p-1.5 px-3 text-white/80 md:p-2.5 md:px-6 rounded-lg md:text-lg font-semibold hover:bg-white/80 hover:text-[rgb(36,36,36)] border-2 border-solid border-transparent hover:border-[rgb(36,36,36)] dark:border-2 dark:border-solid dark:border-white/80 transition space-x-2'
             >
               <span>CV</span> <FiDownload size={20} />
             </Link>
