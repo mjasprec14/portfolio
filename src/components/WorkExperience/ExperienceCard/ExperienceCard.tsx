8b6e27da3332508b448a732bf3c636d14743a153
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-21 20:35:29 +0800

    Feat: WorkExperience/Card Component desktop and mobile responsive

--- src/components/WorkExperience/ExperienceCard/ExperienceCard.tsx
+++ src/components/WorkExperience/ExperienceCard/ExperienceCard.tsx
@@ -8,14 +8,50 @@ type Props = {
 
 const ExperienceCard = ({ experience }: Props) => {
   return (
-    <article>
+    <article className='flex flex-col items-center rounded-lg space-y-7 flex-shrink-0 w-[320px] md:w-[600px] xl:w-[900px]  min-h-[650px] shadow-lg dark:bg-[#292929] p-4 md:p-10 opacity-60 hover:opacity-100 transition-opacity duration-200'>
       <motion.img
         initial={{ y: -100, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1.5 }}
         src={urlFor(experience?.companyImage).url()}
         alt={`${experience?.company} company logo`}
-        className='h-32 w-32 rounded-full object-cover object-center'
+        className='h-24 w-24 md:h-36 md:w-36 rounded-full object-cover object-center'
       />
+      <div className='px-0 md:px-10'>
+        <h4 className='text-xl md:text-4xl font-light'>
+          {experience?.jobTitle}
+        </h4>
+        <p className='md:text-2xl font-semibold md:mt-1 tracking-wider'>
+          {experience?.company}
+        </p>
+
+        <div className='flex flex-wrap space-x-2 my-4'>
+          {experience?.technologies.map((tech) => (
+            <motion.img
+              key={tech?._id}
+              src={urlFor(tech?.image).url()}
+              className='h-6 w-6 md:h-10 md:w-10 rounded-full'
+            />
+          ))}
+        </div>
+
+        <p className='text-xs md:text-sm uppercase pb-1 font-bold text-gray-300'>
+          {experience?.dateStarted} - {experience?.dateEnded}
+        </p>
+
+        <motion.div
+          initial={{ x: 300, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1.5 }}
+          className='h-1 w-4/5 bg-gradient-to-r from-blue-600  via-red-500 to-[#F7AB0A]/80 rounded-xl mb-4'
+        />
+
+        <ul className='text-xs h-1/4 list-disc space-y-4 ml-4 md:text-lg md:h-44 overflow-y-scroll scrollbar-thin md:px-6'>
+          {experience?.points.map((point: string, idx: number) => (
+            <li key={idx}>{point}</li>
+          ))}
+        </ul>
+      </div>
     </article>
   );
 };
--- src/components/WorkExperience/WorkExperience.tsx
+++ src/components/WorkExperience/WorkExperience.tsx
@@ -7,15 +7,10 @@ type Props = {
 };
 
 const WorkExperience = ({ experiences }: Props) => {
-  console.log(experiences);
   return (
     <section id='experience'>
-      <div className='h-screen relative flex flex-col text-left overflow-hidden md:flex-row max-w-full px-10 justify-evenly items-center mx-auto'>
-        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
-          Experience
-        </h3>
-
-        <div>
+      <div className='h-screen relative flex flex-col text-left overflow-hidden md:flex-row max-w-full md:px-10 justify-evenly items-center mx-auto'>
+        <div className='w-full flex space-x-5 overflow-x-scroll p-4 md:p-10 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-[#F7AB0A]/80'>
           {experiences?.map((experience) => (
             <div key={experience?._id}>
               <ExperienceCard experience={experience} />
