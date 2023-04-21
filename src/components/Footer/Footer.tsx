4080590bb50748a872d6de840ef528016bf27ad3
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-21 16:38:17 +0800

    fix: copy and alignments

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import { Social, PageInfo } from '@/src/typings/typings';
 import { motion } from 'framer-motion';
 import { SocialIcon } from 'react-social-icons';
 import { AiFillPhone, AiOutlineMail } from 'react-icons/ai';
+import { useTheme } from 'next-themes';
 
 type Props = {
   socials: Social[];
@@ -10,13 +11,13 @@ type Props = {
 };
 
 const Footer = ({ socials, pageInfo }: Props) => {
+  const { theme } = useTheme();
+
   let socmed = socials.filter(
     (item) =>
       item.title.toLowerCase() !== 'read.cv' &&
       item.title.toLowerCase() !== 'theorg'
   );
-
-  console.log(pageInfo);
 
   return (
     <>
@@ -26,10 +27,10 @@ const Footer = ({ socials, pageInfo }: Props) => {
         transition={{ duration: 1.5 }}
         className='h-1 w-4/5 bg-gradient-to-r from-blue-600  via-red-500 to-[#F7AB0A]/80'
       />
-      <footer className='w-full py-6 px-4 space-y-6 md:px-10 md:py-0 md:h-1/6  flex flex-col md:flex-row justify-between md:items-center '>
-        <div className='flex flex-col space-y-6 md:space-y-0 flex-1 md:flex-row md:flex-grow'>
+      <footer className='w-full py-6 px-4 space-y-6 md:px-10 md:py-0 md:h-1/6  flex flex-col md:flex-row justify-between md:items-start '>
+        <div className='flex flex-col space-y-6 md:space-y-0 flex-1 md:flex-row md:flex-grow md:pt-6'>
           <div className='flex-1 space-y-2'>
-            <h3 className='text-xs text-gray-500 uppercase font-semibold tracking-[2px]'>
+            <h3 className='text-sm text-gray-500 uppercase font-semibold tracking-[2px]'>
               Come and say hello
             </h3>
             <p className='text-xs text-gray-400 uppercase tracking-[2px]'>
@@ -37,7 +38,7 @@ const Footer = ({ socials, pageInfo }: Props) => {
             </p>
           </div>
           <div className='flex flex-grow flex-col md:items-center space-y-4'>
-            <h3 className='text-xs text-gray-500 uppercase font-semibold tracking-[2px]'>
+            <h3 className='text-sm text-gray-500 uppercase font-semibold tracking-[2px]'>
               Get in touch with me
             </h3>
             <div className='flex items-center space-x-3'>
@@ -52,6 +53,8 @@ const Footer = ({ socials, pageInfo }: Props) => {
                       url={social?.url}
                       style={{ height: 36, width: 36 }}
                       aria-label={social?.title}
+                      bgColor={theme === 'dark' ? 'gray' : ''}
+                      fgColor={theme === 'dark' ? 'transparent' : ''}
                     />
                   );
                 }
@@ -75,8 +78,8 @@ const Footer = ({ socials, pageInfo }: Props) => {
           </div>
         </div>
 
-        <div className='flex-shrink mt-16'>
-          <p className='text-xs md:text-sm text-gray-500 uppercase font-semibold tracking-[5px]'>
+        <div className='flex-shrink mt-16 md:mt-0 '>
+          <p className='text-sm text-gray-500 uppercase font-semibold tracking-[5px]'>
             {new Date().getFullYear()} &copy; All Rights Reserved.
           </p>
         </div>
