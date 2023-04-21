96885090e16ed5c12c7bbe13daaedcbfde9aa10e
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-22 01:33:44 +0800

    feat: Experience page using HOC

---
+++ app/experience/page.tsx
@@ -0,0 +1,49 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import { Layout } from '@components';
+import { Experience, PageInfo, Social } from '@/src/typings/typings';
+import { getExperience, getPageInfo, getSocials } from '@utils';
+import { WorkExperience } from '@components';
+
+type Props = {
+  socials: Social[];
+  experiences: Experience[];
+  pageInfo: PageInfo;
+};
+
+const page = async () => {
+  const socials: Social[] = await getSocials();
+  const experiences: Experience[] = await getExperience();
+  const pageInfo: PageInfo = await getPageInfo();
+
+  return {
+    socials,
+    experiences,
+    pageInfo,
+  };
+};
+
+const PageWrapper = () => {
+  const [data, setData] = useState<Props>();
+
+  useEffect(() => {
+    page().then(setData);
+  }, []);
+
+  if (!data) {
+    return null;
+  }
+
+  const { socials, experiences, pageInfo } = data;
+
+  return (
+    <Layout
+      socials={socials}
+      pageInfo={pageInfo}
+    >
+      <WorkExperience experiences={experiences} />
+    </Layout>
+  );
+};
+
+export default PageWrapper;
