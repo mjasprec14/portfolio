1de146d5d831ea16a04160676f7ab5dea33d99c0
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-22 01:33:05 +0800

    feat: Layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+'use client';
+import { PageInfo, Social } from '@/src/typings/typings';
+import { Header, Navbar, Footer } from '@components';
+
+type Prop = {
+  socials: Social[];
+  pageInfo: PageInfo;
+  children: React.ReactNode;
+};
+
+const Layout = ({ children, socials, pageInfo }: Prop) => {
+  return (
+    <main className='z-0 h-screen overflow-x-hidden overflow-y-scroll scrollbar-thin scrollbar-track-transparent scrollbar-thumb-[#F7AB0A]/80 dark:bg-[rgb(36,36,36)] dark:text-white'>
+      <Header socials={socials} />
+      <Navbar />
+      {children}
+      <Footer
+        pageInfo={pageInfo}
+        socials={socials}
+      />
+    </main>
+  );
+};
+
+export default Layout;
--- src/components/index.ts
+++ src/components/index.ts
@@ -5,3 +5,4 @@ export { default as About } from './About/About';
 export { default as TransitionEffect } from './TransitionEffect/TransitionEffect';
 export { default as Footer } from './Footer/Footer';
 export { default as WorkExperience } from './WorkExperience/WorkExperience';
+export { default as Layout } from './Layout/Layout';
