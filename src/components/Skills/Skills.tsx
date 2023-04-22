b63b53af70957647993b3a9d14327016e2db97aa
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-23 02:12:51 +0800

    feat: Skills section/component

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,21 +1,31 @@
+'use client';
 import Icon from './Icon/Icon';
 import { Skill } from '@/src/typings/typings';
+import { motion } from 'framer-motion';
+import { quote } from '../About/About.utils';
+import { iconParent } from './Skills.utils';
 
 type Props = {
   skills: Skill[];
 };
 
 const Skills = ({ skills }: Props) => {
-  console.log(skills);
   return (
-    <section>
-      <div>
-        {skills.map((skill, idx) => (
-          <Icon
-            key={idx}
-            skill={skill}
-          />
-        ))}
+    <section id='skills'>
+      <div className='min-h-screen max-w-7xl flex flex-col justify-center items-center mx-auto'>
+        <motion.div
+          variants={iconParent}
+          initial='initial'
+          whileInView='animate'
+          className='grid px-6 grid-cols-4 gap-3 md:px-0 md:grid-cols-6 md:gap-6'
+        >
+          {skills.map((skill, idx) => (
+            <Icon
+              key={idx}
+              skill={skill}
+            />
+          ))}
+        </motion.div>
       </div>
     </section>
   );
