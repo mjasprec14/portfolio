55616abc09b7e2b9352243d06df96217a63e4963
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-22 22:43:46 +0800

    fix: padding top and bottom for card shadow issue

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -13,7 +13,7 @@ const Project = ({ projects, skills }: Props) => {
   return (
     <section id='project'>
       <div className='my-20 md:my-56 flex flex-col md:px-6 max-w-full justify-center items-center mx-auto'>
-        <div className='w-full md:max-w-screen-2xl flex overflow-x-scroll space-x-5 md:gap-3 xl:gap-y-12 md:space-x-0 md:grid md:grid-cols-4 xl:grid-cols-4 p-4 md:p-0 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-[#F7AB0A]/80'>
+        <div className='w-full md:max-w-screen-xl flex overflow-x-scroll space-x-5 md:gap-3 xl:gap-y-10 md:space-x-0 md:grid md:grid-cols-4 xl:grid-cols-4 p-4 md:p-0 md:py-6 scrollbar-thin scrollbar-track-transparent scrollbar-thumb-[#F7AB0A]/80'>
           {myArr.map((item) => (
             <ProjectCard
               key={item}
